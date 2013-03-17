01de8963342fcd53e8d2bbc0fe4612f91d0540db
cjp <cornwarecjp@lavabit.com>
2013-03-17 11:03:38 +0100

    Added test file for amikoComLink class

---
+++ test/amikocomlink_tests.cpp
@@ -0,0 +1,39 @@
+/*
+    key_tests.cpp
+    Copyright (C) 2013 by CJP
+
+    This file is part of Amiko Pay.
+
+    Amiko Pay is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Amiko Pay is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Amiko Pay. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <cstdio>
+
+#include "amikocomlink.h"
+#include "test.h"
+
+/*
+TODO: document and expand
+*/
+class CAmikoComLinkTest : public CTest
+{
+	virtual const char *getName()
+		{return "amikocomlink";}
+
+	virtual void run()
+	{
+		//TODO
+	}
+} amikoComLinkTest;
+
