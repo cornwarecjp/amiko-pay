27c25b8021b59918c8b749ef01f0ff32a13eb4ae
cjp <cornwarecjp@lavabit.com>
2013-04-01 12:17:24 +0200

    Added a commandline user interface

--- main.cpp
+++ main.cpp
@@ -18,6 +18,8 @@
     along with Amiko Pay. If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <cstdio>
+
 #include <openssl/ssl.h>
 #include <openssl/err.h>
 
@@ -27,16 +29,81 @@
 
 #include "amiko.h"
 #include "timer.h"
+#include "key.h"
+
+CString getInput(CString question="")
+{
+	printf("%s", question.c_str());
+	CString ret;
+	while(true)
+	{
+		char c = getchar();
+		if(c == '\n') break;
+		ret += c;
+	}
+	return ret;
+}
+
 
 void app()
 {
 	CAmiko amiko(CConfFile("amikopay.conf"));
 	amiko.start();
 
-	CTimer::sleep(10000); //10 s
+	//Wait some time to allow initialization to finish
+	CTimer::sleep(1000);
+
+	printf(
+		"\n"
+		"\n"
+		"Amiko Pay Copyright (C) 2013\n"
+		"\n"
+		"Amiko Pay is free software: you can redistribute it and/or modify\n"
+		"it under the terms of the GNU General Public License as published by\n"
+		"the Free Software Foundation, either version 3 of the License, or\n"
+		"(at your option) any later version.\n"
+		"\n"
+		"Amiko Pay is distributed in the hope that it will be useful,\n"
+		"but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+		"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
+		"GNU General Public License for more details.\n"
+		"\n"
+		"You should have received a copy of the GNU General Public License\n"
+		"along with Amiko Pay. If not, see <http://www.gnu.org/licenses/>.\n"
+		"\n"
+		"Enter \"help\" for a list of commands.\n"
+		);
+
+	while(true)
+	{
+		CString input = getInput("> ");
+		if(input == "quit" || input == "exit") break;
+		if(input == "newkey")
+		{
+			CKey key;
+			key.makeNewKey();
+
+			printf("localPrivateKey = %s\n", key.getPrivateKey().hexDump().c_str());
+			printf("remoteURI = %s\n", amiko.getSettings().getLocalURL(key).c_str());
+			printf("remotePublicKey = %s\n", key.getPublicKey().hexDump().c_str());
+		}
+		if(input == "help")
+		{
+			printf(
+				"exit:\n"
+				"quit:\n"
+				"  Terminate application.\n"
+				"help:\n"
+				"  Display this message.\n"
+				"newkey:\n"
+				"  Make a new key pair and display its properties.\n"
+				);
+		}
+	}
 
 	amiko.stop();
 }
+
 
 int main(int argc, char **argv)
 {
