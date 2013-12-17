1c3112f5eb11202b33492be83d853688561e0309
cjp <cornwarecjp@lavabit.com>
2013-12-17 22:02:57 +0100

    Fixed port numbers because of the ports needed for payments

--- test/amiko_tests.cpp
+++ test/amiko_tests.cpp
@@ -54,9 +54,11 @@ class CAmikoTest : public CTest
 		CAmikoSettings settings1, settings2;
 		settings1.m_linkHostname = "localhost";
 		settings1.m_linkPortNumber = "12345";
+		settings1.m_paymentPortNumber = "12346";
 		settings1.m_linksDir = "./testlinks/amiko1/"; //final slash is necessary
 		settings2.m_linkHostname = "localhost";
-		settings2.m_linkPortNumber = "12346";
+		settings2.m_linkPortNumber = "12347";
+		settings2.m_paymentPortNumber = "12348";
 		settings2.m_linksDir = "./testlinks/amiko2/"; //final slash is necessary
 
 		CFile::makeDirectory(settings1.m_linksDir);
