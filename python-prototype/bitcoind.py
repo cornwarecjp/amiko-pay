c650853c08fcf68486f815e7ed9a1b2cca61e9e4
cornwarecjp <cornwarecjp@ultimatestunts.nl>
2014-10-05 14:54:14 +0200

    Added docstrings to the Bitcoind class, to aid in porting Amiko to other Bitcoin software

--- python-prototype/bitcoind.py
+++ python-prototype/bitcoind.py
@@ -25,7 +25,19 @@ import log
 
 
 class Bitcoind:
+	"""
+	Connection to a Bitcoin daemon process.
+	"""
+
 	def __init__(self, settings):
+		"""
+		Arguments:
+		settings: a settings object; must contain the attribute bitcoinRPCURL.
+
+		Connects to a Bitcoin daemon process, indicated by settings.bitcoinRPCURL.
+		If settings.bitcoinRPCURL is empty, this object will not be connected.
+		"""
+
 		if settings.bitcoinRPCURL != "":
 			log.log("Making connection to Bitcoin daemon...")
 			self.access = AuthServiceProxy(settings.bitcoinRPCURL)
@@ -36,32 +48,104 @@ class Bitcoind:
 
 
 	def isConnected(self):
+		"""
+		Return value:
+		bool
+
+		Returns whether this object is connected.
+		"""
+
 		return self.access != None
 		
 
 	def getBalance(self):
+		"""
+		Return value:
+		int, in Satoshi
+
+		Returns the balance.
+		"""
+
 		return self.DecimaltoAmount(self.access.getbalance())
 
 
 	def getBlockCount(self):
+		"""
+		Return value:
+		int
+
+		Returns the block count.
+		"""
+
 		return self.access.getblockcount()
 
 
 	def getPrivateKey(self, address):
+		"""
+		Arguments:
+		address: str, Base58Check-encoded address
+		Return value:
+		str, Base58Check-encoded private key
+
+		Returns the private key corresponding to the given address.
+		"""
+
 		return self.access.dumpprivkey(address)
 
 
 	def getTransactionHashesByBlockHeight(self, height):
+		"""
+		Arguments:
+		height: int
+		Return value:
+		list of str, hexadecimal, Bitcoin hash byte order
+
+		Returns the transaction hashes in the block (in the main chain) at the
+		given height.
+		"""
+
 		bhash = self.access.getblockhash(height)
 		block = self.access.getblock(bhash)
 		return block["tx"]
 
 
 	def getTransaction(self, thash):
+		"""
+		Arguments:
+		thash: str, hexadecimal, Bitcoin hash byte order
+
+		Return value:
+		dict, containing:
+			vin: list of dict, each element containing:
+				coinbase [only for coinbase transactions]
+				txid [only for non-coinbase transactions]:
+					str, hexadecimal, Bitcoin hash byte order
+					hash of input transaction
+
+		Returns information about the transaction indicated by the given hash.
+		"""
+
 		return self.access.getrawtransaction(thash, 1)
 
 
 	def listUnspent(self):
+		"""
+		Return value:
+		list of dict, each element containing:
+			address: 
+				str, Base58Check-encoded address
+			amount:
+				int, in Satoshi
+			scriptPubKey:
+				str, binary
+			txid:
+				str, binary, OpenSSL byte order
+			vout:
+				int
+
+		Returns information about the available unspent transaction outputs.
+		"""
+
 		ret = self.access.listunspent()
 		for vout in ret:
 			vout["txid"] = binascii.unhexlify(vout["txid"])[::-1] #reversed; TODO: is this the right place?
@@ -71,6 +155,13 @@ class Bitcoind:
 
 
 	def sendRawTransaction(self, txData):
+		"""
+		Arguments:
+		txData: str, binary
+
+		Send the given serialized transaction over the Bitcoin network.
+		"""
+
 		self.access.sendrawtransaction(txData.encode("hex"))
 
 
