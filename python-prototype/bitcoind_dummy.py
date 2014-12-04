22d09af66cd062c8df3777c48de2fad3756b8334
cornwarecjp <cornwarecjp@ultimatestunts.nl>
2014-12-04 19:54:24 +0100

    Implemented enough of the dummy bitcoind to allow the deposit and withdrawal code to pass

--- python-prototype/bitcoind_dummy.py
+++ python-prototype/bitcoind_dummy.py
@@ -17,8 +17,12 @@
 #    along with Amiko Pay. If not, see <http://www.gnu.org/licenses/>.
 
 import binascii
+import random
 
 import log
+
+from crypto import *
+import base58
 
 
 
@@ -28,7 +32,9 @@ class Bitcoind_Dummy:
 	"""
 
 	def __init__(self, settings):
-		pass
+		self.random = random.Random()
+		self.random.seed(42)
+		self.keys = []
 
 
 	def isConnected(self):
@@ -44,11 +50,18 @@ class Bitcoind_Dummy:
 
 
 	def getNewAddress(self):
-		raise Exception("Not yet implemented")
+		return self.__getAddressFromKey(self.__makeNewKey())
 
 
 	def getPrivateKey(self, address):
-		raise Exception("Not yet implemented")
+		for k in self.keys:
+			if self.__getAddressFromKey(k) == address:
+				return base58.encodeBase58Check(k.getPrivateKey(), 128)
+
+		while True:
+			k = self.__makeNewKey()
+			if self.__getAddressFromKey(k) == address:
+				return base58.encodeBase58Check(k.getPrivateKey(), 128)
 
 
 	def getTransactionHashesByBlockHeight(self, height):
@@ -60,10 +73,46 @@ class Bitcoind_Dummy:
 
 
 	def listUnspent(self):
-		raise Exception("Not yet implemented")
+		k1 = self.__makeNewKey()
+		k2 = self.__makeNewKey()
+		return \
+		[
+			{
+			"address": self.__getAddressFromKey(k),
+			"amount": 1000000000,
+			"scriptPubKey": "",
+			"txid": SHA256(k.getPublicKey()), #Wrong, but good enough for testing
+			"vout": 0
+			}
+
+			for k in [k1, k2]
+		]
 
 
 	def sendRawTransaction(self, txData):
-		raise Exception("Not yet implemented")
+		pass
 
+
+	def __makeNewKey(self):
+		newKey = Key()
+		privKey = "".join([chr(self.random.getrandbits(8)) for i in range(32)])
+		privKey += "\0" #indicate the use of compressed keys
+		newKey.setPrivateKey(privKey)
+		self.keys.append(newKey)
+		return newKey
+
+
+	def __getAddressFromKey(self, key):
+		return base58.encodeBase58Check(
+			RIPEMD160(SHA256(key.getPublicKey())),
+			0)
+
+
+
+if __name__ == "__main__":
+	#Test:
+	bitcoind = Bitcoind_Dummy(None)
+	for i in range(10):
+		a = bitcoind.getNewAddress()
+		print a, bitcoind.getPrivateKey(a)
 
