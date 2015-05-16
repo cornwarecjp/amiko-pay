e2ea53428ed2afffb8cc3ee7a0fce9a3ea6355a5
cornwarecjp <cornwarecjp@ultimatestunts.nl>
2015-05-16 12:26:23 +0200

    Added BOTG spending function

--- python-prototype/goodies/botg.py
+++ python-prototype/goodies/botg.py
@@ -27,13 +27,32 @@
 #    such a combination shall include the source code for the parts of the
 #    OpenSSL library used as well as that of the covered work.
 
+import binascii
+import decimal
 import sys
 sys.path.append(".")
 sys.path.append("..")
 
 from amiko.utils.crypto import Key, SHA256, RIPEMD160
 from amiko.utils import base58
+from amiko.utils import bitcointransaction as btx
 
+
+
+mBTC = 100000 #Satoshi
+
+
+def readPrivateKey(filename):
+	with open(filename, "rb") as f:
+		privateKey = f.read()
+	privateKey = privateKey.split("\n")[0] #first line
+	privateKey = privateKey.strip() #ignore whitespace
+	return base58.decodeBase58Check(privateKey, 128) #PRIVKEY = 128
+
+
+def getAddress(key):
+	publicKeyHash = RIPEMD160(SHA256(key.getPublicKey()))
+	return base58.encodeBase58Check(publicKeyHash, 0) #PUBKEY_ADDRESS = 0
 
 
 def makekey(args):
@@ -43,8 +62,7 @@ def makekey(args):
 	privateKey = k.getPrivateKey()
 	privateKey = base58.encodeBase58Check(privateKey, 128) #PRIVKEY = 128
 
-	publicKeyHash = RIPEMD160(SHA256(k.getPublicKey()))
-	address = base58.encodeBase58Check(publicKeyHash, 0) #PUBKEY_ADDRESS = 0
+	address = getAddress(k)
 
 	with open(address, "wb") as f:
 		f.write(privateKey + "\n")
@@ -55,27 +73,105 @@ def getinfo(args):
 	for filename in args:
 		print "----------------"
 		print "Filename: ", filename
-		with open(filename, "rb") as f:
-			privateKey = f.read()
-		privateKey = privateKey.split("\n")[0] #first line
-		privateKey = privateKey.strip() #ignore whitespace
-		privateKey = base58.decodeBase58Check(privateKey, 128) #PRIVKEY = 128
+		privateKey = readPrivateKey(filename)
 		k = Key()
 		k.setPrivateKey(privateKey)
-		publicKey = k.getPublicKey()
-		publicKeyHash = RIPEMD160(SHA256(publicKey))
-		print "Public key: ", publicKey.encode("hex")
-		print "Address: ", base58.encodeBase58Check(publicKeyHash, 0) #PUBKEY_ADDRESS = 0
+		print "Public key: ", k.getPublicKey().encode("hex")
+		print "Address: ", getAddress(k)
 
 
+def spend(args):
+	#Load the keys
+	keys = []
+	for filename in args:
+		privateKey = readPrivateKey(filename)
+		k = Key()
+		k.setPrivateKey(privateKey)
+		keys.append(k)
 
-#TODO: other BOTG-functions, like making and signing a transaction
+	def getKey(question):
+		for i in range(len(keys)):
+			print i+1, getAddress(keys[i])
+		i = int(raw_input(question)) - 1
+		return keys[i]
+
+	#Ask for input information:
+	inputs = []
+	totalAmount = 0
+	while True:
+		txid = raw_input("Transaction ID of unspent output (Enter to stop): ")
+		txid = txid.strip()
+		if txid == "":
+			break
+		txid = binascii.unhexlify(txid)[::-1]
+
+		vout = int(raw_input("Output index of unspent output: "))
+		k = getKey("Address of unspent output: ")
+		inputs.append((txid, vout, k))
+
+		totalAmount += int(decimal.Decimal(
+			raw_input("Amount in unspent output (mBTC): ")
+			) * mBTC)
+
+	print "Total of amounts: %s mBTC" % str(decimal.Decimal(totalAmount)/mBTC)
+
+	fee = int(0.01 * mBTC)
+	print "Transaction fee is set to: %s mBTC" % str(decimal.Decimal(fee)/mBTC)
+
+	destAddress = raw_input("Destination address: ")
+	destHash = base58.decodeBase58Check(destAddress, 0) #PUBKEY_ADDRESS = 0
+	destAmount = int(decimal.Decimal(
+			raw_input("Amount to send to destination (mBTC): ")
+			) * mBTC)
+	if destAmount < 0:
+		print "Negative amount is not allowed"
+		sys.exit(2)
+	if destAmount > totalAmount - fee:
+		print "Not enough funds"
+		sys.exit(1)
+
+	changeKey = getKey("Address to send change amount to: ")
+	changeAddress = getAddress(changeKey)
+	changeHash = base58.decodeBase58Check(changeAddress, 0) #PUBKEY_ADDRESS = 0
+
+	tx = btx.Transaction(
+		tx_in = [
+			btx.TxIn(x[0], x[1])
+			for x in inputs
+			],
+		tx_out = [
+			btx.TxOut(destAmount, btx.Script.standardPubKey(destHash))
+			]
+		)
+	changeAmount = totalAmount - destAmount - fee
+	if changeAmount < 0:
+		raise Exception("Error: got negative change amount")
+	elif changeAmount == 0:
+		print "Note: change amount is zero - no change is sent"
+	else:
+		tx.tx_out.append(
+			btx.TxOut(changeAmount, btx.Script.standardPubKey(changeHash))
+			)
+
+	for i in range(len(inputs)):
+		#print tx.tx_in[i].previousOutputHash.encode("hex"), tx.tx_in[i].previousOutputIndex
+		key = inputs[i][2]
+		address = getAddress(key)
+		hash = base58.decodeBase58Check(address, 0) #PUBKEY_ADDRESS = 0
+		scriptPubKey = btx.Script.standardPubKey(hash)
+		tx.signInput(i, scriptPubKey, [None, key.getPublicKey()], [key])
+
+	print "Serialized transaction:"
+	print tx.serialize().encode("hex")
+	print "Transaction ID:", tx.getTransactionID()[::-1].encode("hex")
+
 
 
 funcs = \
 {
 "makekey": makekey,
-"getinfo": getinfo
+"getinfo": getinfo,
+"spend": spend
 }
 funcNames = funcs.keys()
 funcNames.sort()
